399de9d5a433e8ded9bd5cd23ceeb4b18032f681
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 01:51:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ attendees.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigne
 end
 
 def printer(attendees)
- batch_badge_creator(attendees).each do puts badges
+ batch_badge_creator(attendees).each do puts badges.each
   end
 end
