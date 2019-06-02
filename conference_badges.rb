2b3d10db5904f28c887d64ce771a2ca98df82e49
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 01:35:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ def assign_rooms(attendees)
 attendees.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
 
-def printer
-batch_badge_creator.each do
-puts batch_badge_creator
+def printer(attendees)
+batch_badge_creator(attendees).each do
+puts batch_badge_creator(attendees)
 end
 end
